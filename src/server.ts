3f7f72a4a496a52e4acd3ac70b4c490bd4c681ef
Nikolay <bulian1311@gmail.com>
2020-09-04 11:58:12 +0300

    typescript support.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const server = express();
 
-server.listen(
-  process.env.PORT || 4000,
+server.listen(process.env.PORT || 4000, () =>
   console.log(
     `Сервер запущен в ${process.env.NODE_ENV} моде, на порте ${process.env.PORT}`
   )
