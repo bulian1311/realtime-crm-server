de0677b15fa677168bc88cf664e8337f39290865
Nikolay <bulian1311@gmail.com>
2020-09-05 12:36:00 +0300

    add helmet midleware

--- src/express/index.ts
+++ src/express/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 
 const expressApp = express();
 
 expressApp.use(cors());
+expressApp.use(helmet());
 
 expressApp.get("/", (req, res) => {
   res.send("Hello");
